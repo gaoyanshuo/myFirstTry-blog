18065e9a589be035898713df8c890146c5f99daf
gaoyanshuo <gaoyanshuo9388@yahoo.com>
2021-07-12 22:43:58 +0900

    添加路由别名

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaticPagesController;
 
 
+//Auth::routes();
+//
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/',[StaticPagesController::class,'home'])->name('home');
+Route::get('/help',[StaticPagesController::class,'help'])->name('help');
+Route::get('/about',[StaticPagesController::class,'about'])->name('about');
 
 
-Route::get('/',[StaticPagesController::class,'home']);
-Route::get('/help',[StaticPagesController::class,'help']);
-Route::get('/about',[StaticPagesController::class,'about']);
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
