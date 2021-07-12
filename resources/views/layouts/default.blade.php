31fde9cc4218baa6435c8d153beeece07dfead6d
gaoyanshuo <gaoyanshuo9388@yahoo.com>
2021-07-13 02:27:34 +0900

    登陆成功后/注册成功后逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,10 +7,11 @@
 <body>
   @include('layouts._header')
 
-<div class="container">
-  @include('shared._messages')
-  @yield('content')
-  @include('layouts._footer')
-</div>
+  <div class="container">
+    @include('shared._messages')
+    @yield('content')
+    @include('layouts._footer')
+  </div>
+  <script src="/js/app.js"></script>
 </body>
 </html>
