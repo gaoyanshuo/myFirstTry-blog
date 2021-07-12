d38167f4e433e076a21fa6610dd276739876c438
gaoyanshuo <gaoyanshuo9388@yahoo.com>
2021-07-12 22:38:58 +0900

    将header/footer切割，单独引用

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="#" target=_blank>
+    Day Day Up
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >关于</a>
+  </div>
+</footer>
