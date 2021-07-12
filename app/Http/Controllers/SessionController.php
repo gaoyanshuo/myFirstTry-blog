41b10e1f2a9aae3f4ecf9fad2ac25169a8bdc8bf
gaoyanshuo <gaoyanshuo9388@yahoo.com>
2021-07-13 02:13:08 +0900

    创建会话

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller
 {
@@ -13,10 +15,17 @@ class SessionController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request,[
+        $credentials = $this->validate($request,[
             'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
-        return;
+        if (Auth::attempt($credentials)) {
+            session()->flash('success','ようこそ');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger','邮箱或密码不正确');
+            return redirect()->back()->withInput();
+        }
+
     }
 }
