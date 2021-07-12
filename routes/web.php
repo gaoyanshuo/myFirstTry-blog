a758cf0de6794051364490a478f843d662e314c5
gaoyanshuo <gaoyanshuo9388@yahoo.com>
2021-07-12 22:53:04 +0900

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaticPagesController;
-
+use App\Http\Controllers\UsersController;
 
 //Auth::routes();
 //
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+//homepage
+
 Route::get('/',[StaticPagesController::class,'home'])->name('home');
 Route::get('/help',[StaticPagesController::class,'help'])->name('help');
 Route::get('/about',[StaticPagesController::class,'about'])->name('about');
 
+//signup
+
+Route::get('/signup',[UsersController::class,'create'])->name('signup');
 
