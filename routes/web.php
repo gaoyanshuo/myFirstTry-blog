a1bc8b7cbf286c3ca9e399e4a6f617f0a6cdb919
gaoyanshuo <gaoyanshuo9388@yahoo.com>
2021-07-14 22:37:59 +0900

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -13,40 +13,36 @@ use App\Http\Controllers\StatusesController;
 
 
 //homepage
-
 Route::get('/',[StaticPagesController::class,'home'])->name('home');
 Route::get('/help',[StaticPagesController::class,'help'])->name('help');
 Route::get('/about',[StaticPagesController::class,'about'])->name('about');
 
 //signup
-
 Route::get('/signup',[UsersController::class,'create'])->name('signup');
 
 //user_CRUD
-
 Route::resource('users', UsersController::class);
 
 //show
-
 Route::get('/users/{user}', [UsersController::class,'show'])->name('users.show');
 
 //login-logout
-
 Route::get('login', [SessionController::class,'create'])->name('login');
 Route::post('login', [SessionController::class,'store'])->name('login');
 Route::delete('logout', [SessionController::class,'destroy'])->name('logout');
 
 //token
-
 Route::get('signup/confirm/{token}',[UsersController::class,'confirmEmail'])->name('confirm_email');
 
 //change_user_password
-
 Route::get('/password/reset',[PasswordController::class,'showLinkRequestForm'])->name('password.request');
 Route::post('/password/email',[PasswordController::class,'sendResetLinkEmail'])->name('password.email');
 Route::get('/password/reset/{token}',[PasswordController::class,'showResetForm'])->name('password.reset');
 Route::post('/password/reset',[PasswordController::class,'reset'])->name('password.update');
 
 //statuses
-
 Route::resource('statuses', StatusesController::class,['only' => ['store','destroy']]);
+
+//获取关注数,粉丝数
+Route::get('/user/{user}/following',[UsersController::class,'followings'])->name('users.followings');
+Route::get('/user/{user}/followers',[UsersController::class,'followers'])->name('users.followers');
